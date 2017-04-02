9f82fca3c5a44026a3ff4ea5fa3200dbe456fd5a
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-04-02 20:49:32 +0200

    implement HITs algorithm

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.asTitle = undefined;
+
+require('babel-polyfill');
+
 var _fs = require('fs');
 
 var _mathjs = require('mathjs');
@@ -8,18 +15,36 @@ var math = _interopRequireWildcard(_mathjs);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 // arguments from command line
 var args = process.argv.slice(2);
-var INUPT = args[0] || 'graph.json'; // read json from here
-var OUTPUT = args[1]; // write here (if falsey, console.log)
+var _ref = [args[0] || 'graph.json', args[1]],
+    INUPT = _ref[0],
+    OUTPUT = _ref[1];
 
-// returns a table-like pretty string for a given matrix of numbers
-var prettify = function prettify(matrix) {
-  return matrix.map(function (row, i) {
+// for prettier printing
+
+var prettifyMatrix = function prettifyMatrix(matrix) {
+  return matrix.map(function (row) {
     return row.map(function (n) {
       return n.toFixed(4);
     }).join('  ');
   }).join('\n');
+};
+var prettifyVector = function prettifyVector(vector) {
+  return vector.map(function (x) {
+    return x.toFixed(6);
+  }).join('\n');
+};
+var trim = function trim(string) {
+  return string.replace(/\n\s*/g, '\n');
+};
+var repeat = function repeat(char, times) {
+  return new Array(times).fill(char).join('');
+};
+var asTitle = exports.asTitle = function asTitle(title) {
+  return trim('\n\n\u2554\u2550' + repeat('═', 76) + '\u2550\u2557\n                                          \u2551 ' + title.padEnd(76) + ' \u2551\n                                          \u255A\u2550' + repeat('═', 76) + '\u2550\u255D\n');
 };
 
 var ALPHA = .9; // probability of using teleport
@@ -27,17 +52,15 @@ var graph = JSON.parse((0, _fs.readFileSync)(INUPT, 'utf8'));
 var getNodes = function getNodes(graph) {
   return Object.keys(graph);
 }; // gets array of node names
+var getLength = function getLength(graph) {
+  return getNodes(graph).length;
+};
 
-// transforms json strcture to a matrix with filed probabilities of navigation
-// for example, for   A -> B, C    B -> A    C -> A   the result is:
-// [ [ 0.0, 0.5, 0.5 ]     // from A to A, B, C
-//   [ 1.0, 0.0, 0.0 ]     // from B to A, B, C
-//   [ 1.0, 0.0, 0.0 ] ]   // from C to A, B, C
+// transforms json to a matrix with filled probabilities of navigation
 var toMatrix = function toMatrix(graph) {
   var nodes = getNodes(graph);
   return nodes.map(function (node) {
-    var numberOfNeighbors = graph[node].length;
-    var probability = 1 / numberOfNeighbors;
+    var probability = 1 / graph[node].length;
     return nodes.map(function (flag) {
       return graph[node].includes(flag) ? probability : 0;
     });
@@ -46,13 +69,13 @@ var toMatrix = function toMatrix(graph) {
 
 var matrix = toMatrix(graph);
 
-var N = getNodes(graph).length;
-var totalProbability = function totalProbability(pl) {
+// const N = getNodes(graph).length
+var totalProb = function totalProb(N, pl) {
   return ALPHA / N + (1 - ALPHA) * pl;
 };
 var transitionMatrix = function transitionMatrix(PL) {
   return PL.map(function (row) {
-    return row.map(totalProbability);
+    return row.map(totalProb.bind(null, PL.length));
   });
 };
 
@@ -83,7 +106,6 @@ var getRang = function getRang(initial, P) {
 };
 
 // for example (4, 0) => [1, 0, 0, 0]
-// or          (3, 1) => [0, 1, 0]
 var getVectorByInitialPage = function getVectorByInitialPage(length, index) {
   var zeros = new Array(length).fill(0);
   return zeros.map(function (x, i) {
@@ -91,20 +113,57 @@ var getVectorByInitialPage = function getVectorByInitialPage(length, index) {
   });
 };
 // for example (4) => [.25, .25, .25, .25]
-// or          (2) => [.5, .5]
 var getEqualVector = function getEqualVector(length) {
   return new Array(length).fill(1 / length);
 };
-var rang = getRang(getEqualVector(N), transitionMatrix(matrix));
+var rang = getRang(getEqualVector(getLength(graph)), transitionMatrix(matrix));
 
-// write an awesome report
-var report = 'REPORT\n\n=== Transition matrix P ===\n' + prettify(transitionMatrix(matrix), getNodes(graph)) + '\n\n=== Rank of pages ===\n' + rang.map(function (x) {
-  return x.toFixed(6);
-}).join('\n') + '\n';
+var toAdjacencyMatrix = function toAdjacencyMatrix(graph) {
+  var nodes = getNodes(graph);
+  return nodes.map(function (node) {
+    return nodes.map(function (f) {
+      return graph[node].includes(f) ? 1 : 0;
+    });
+  });
+};
+var normalize = function normalize(vector) {
+  return vector.map(function (el) {
+    return el / Math.hypot.apply(Math, _toConsumableArray(vector));
+  });
+};
+var normalizedMul = function normalizedMul(a, b) {
+  return normalize(math.multiply(a, b));
+};
+var vectorOnes = function vectorOnes(length) {
+  return new Array(length).fill(1);
+};
+var HITs = function HITs(graph) {
+  var A = toAdjacencyMatrix(graph);
+  var At = math.transpose(A);
+  var _ref2 = [vectorOnes(A.length), vectorOnes(At.length)],
+      a = _ref2[0],
+      h = _ref2[1];
 
-// write to fil or console depending on process args given to program
-if (OUTPUT) {
-  (0, _fs.writeFileSync)(OUTPUT, 'utf-8');
-} else {
-  console.log(report);
-}
+  var aprev = void 0,
+      hprev = void 0;
+  var _ref3 = [math.multiply(A, At), math.multiply(At, A)],
+      AAt = _ref3[0],
+      AtA = _ref3[1];
+
+  do {
+    ;aprev = a;
+    hprev = h;
+    var _ref4 = [normalizedMul(AAt, aprev), normalizedMul(AtA, hprev)];
+    a = _ref4[0];
+    h = _ref4[1];
+  } while (!hasConvergedVector(aprev, a) && !hasConvergedVector(hprev, h));
+  return { a: a, h: h };
+};
+
+var _HITs = HITs(graph),
+    a = _HITs.a,
+    h = _HITs.h;
+
+var report = [asTitle('transitionMatrix'), prettifyMatrix(transitionMatrix(matrix), getNodes(graph)), asTitle('Rank of pages'), prettifyVector(rang), asTitle('Vector a'), prettifyVector(a), asTitle('Vector h'), prettifyVector(h), ''].join('\n');
+
+OUTPUT && (0, _fs.writeFileSync)(OUTPUT, report, 'utf-8') || console.log(report);
--- src/index.js
+++ src/index.js
@@ -1,46 +1,47 @@
+import 'babel-polyfill'
 import {readFileSync as read, writeFileSync as write} from 'fs'
 import * as math from 'mathjs'
 
 // arguments from command line
 const args = process.argv.slice(2)
-const INUPT = args[0] || 'graph.json' // read json from here
-const OUTPUT = args[1]                // write here (if falsey, console.log)
+const [INUPT, OUTPUT] = [args[0] || 'graph.json', args[1]]
 
-// returns a table-like pretty string for a given matrix of numbers
-const prettify = (matrix) =>
-  matrix.map((row, i) => row.map(n => n.toFixed(4)).join('  ')).join('\n')
+// for prettier printing
+const prettifyMatrix = matrix =>
+  matrix.map(row => row.map(n => n.toFixed(4)).join('  ')).join('\n')
+const prettifyVector = vector => vector.map(x => x.toFixed(6)).join('\n')
+const trim = string => string.replace(/\n\s*/g, '\n')
+const repeat = (char, times) => new Array(times).fill(char).join('')
+export const asTitle = title => trim(`\n\n╔═${repeat('═', 76) }═╗
+                                          ║ ${title.padEnd(76)} ║
+                                          ╚═${repeat('═', 76) }═╝\n`)
 
 const ALPHA = .9 // probability of using teleport
 const graph = JSON.parse(read(INUPT, 'utf8'))
 const getNodes = graph => Object.keys(graph)  // gets array of node names
+const getLength = graph => getNodes(graph).length
 
-// transforms json strcture to a matrix with filed probabilities of navigation
-// for example, for   A -> B, C    B -> A    C -> A   the result is:
-// [ [ 0.0, 0.5, 0.5 ]     // from A to A, B, C
-//   [ 1.0, 0.0, 0.0 ]     // from B to A, B, C
-//   [ 1.0, 0.0, 0.0 ] ]   // from C to A, B, C
+// transforms json to a matrix with filled probabilities of navigation
 const toMatrix = graph => {
   const nodes = getNodes(graph)
   return nodes.map(node => {
-    const numberOfNeighbors = graph[node].length
-    const probability = 1 / numberOfNeighbors
+    const probability = 1 / graph[node].length
     return nodes.map(flag => graph[node].includes(flag) ? probability : 0)
   })
 }
 
 const matrix = toMatrix(graph)
 
-const N = getNodes(graph).length
-const totalProbability = pl => (ALPHA / N) + (1 - ALPHA) * pl
-const transitionMatrix = PL => PL.map(row => row.map(totalProbability))
+// const N = getNodes(graph).length
+const totalProb = (N, pl) => (ALPHA / N) + (1 - ALPHA) * pl
+const transitionMatrix = PL => PL.map(row => row.map(totalProb.bind(null, PL.length)))
 
 // test if two floats can be considered as equal based on eps value
 const isEqual = (eps, a, b) => Math.abs(a - b) < eps
 const hasConverged = isEqual.bind(this, 0.001) // eps =  0.001
 
 // check if all values in two vectors have converged
-const hasConvergedVector = (a, b) =>
-  a.every((item, i) => hasConverged(item, b[i]))
+const hasConvergedVector = (a, b) => a.every((item, i) => hasConverged(item, b[i]))
 
 // iterate until vectors have convereged by the given formula
 const getRang = (initial, P) => {
@@ -54,29 +55,40 @@ const getRang = (initial, P) => {
 }
 
 // for example (4, 0) => [1, 0, 0, 0]
-// or          (3, 1) => [0, 1, 0]
 const getVectorByInitialPage = (length, index) => {
   let zeros = new Array(length).fill(0)
   return zeros.map((x, i) => index == i ? 1 : 0)
 }
 // for example (4) => [.25, .25, .25, .25]
-// or          (2) => [.5, .5]
 const getEqualVector = length => new Array(length).fill(1 / length)
-const rang = getRang(getEqualVector(N), transitionMatrix(matrix))
+const rang = getRang(getEqualVector(getLength(graph)), transitionMatrix(matrix))
 
-// write an awesome report
-const report = `REPORT
-
-=== Transition matrix P ===
-${prettify(transitionMatrix(matrix), getNodes(graph))}
-
-=== Rank of pages ===
-${rang.map(x => x.toFixed(6)).join('\n')}
-`
-
-// write to fil or console depending on process args given to program
-if (OUTPUT) {
-  write(OUTPUT, 'utf-8')
-} else {
-  console.log(report)
+const toAdjacencyMatrix = graph => {
+  const nodes = getNodes(graph)
+  return nodes.map(node => nodes.map(f => graph[node].includes(f) ? 1 : 0))
 }
+const normalize = vector => vector.map(el => el / Math.hypot(...vector))
+const normalizedMul = (a, b) => normalize(math.multiply(a, b))
+const vectorOnes = length => new Array(length).fill(1)
+const HITs = graph => {
+  const A = toAdjacencyMatrix(graph)
+  const At = math.transpose(A)
+  let [a, h] = [vectorOnes(A.length), vectorOnes(At.length)]
+  let aprev, hprev
+  let [AAt, AtA] = [math.multiply(A, At), math.multiply(At, A)]
+  do {
+    ;[aprev, hprev] = [a, h]
+    ;[a, h] = [normalizedMul(AAt, aprev), normalizedMul(AtA, hprev)]
+  } while (!hasConvergedVector(aprev, a) && !hasConvergedVector(hprev, h))
+  return {a, h}
+}
+const {a, h} = HITs(graph)
+
+const report = [ asTitle('transitionMatrix'),
+  prettifyMatrix(transitionMatrix(matrix), getNodes(graph)),
+  asTitle('Rank of pages'), prettifyVector(rang),
+  asTitle('Vector a'), prettifyVector(a),
+  asTitle('Vector h'), prettifyVector(h), ''
+].join('\n')
+
+OUTPUT && write(OUTPUT, report, 'utf-8') || console.log(report)
